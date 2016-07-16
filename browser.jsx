70d16d1ee81d08acf5c802f260b313afa4493fc5
Arley Schrock <arleyschrock@outlook.com>
2016-07-15 19:12:22 -0500

    .

--- browser.jsx
+++ browser.jsx
@@ -7,7 +7,7 @@ var urllib = require('url')
 
 function createPageObject (location) {
   return {
-    location: location||'https://github.com/pfraze/electron-browser',
+    location: location||'https://start.me',
     statusText: false,
     title: 'new tab',
     isLoading: false,
